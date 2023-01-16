c71f5de2add6c884d89e52892def099394195937
SzymanskiKarol <szymanski.karol.1991@gmail.com>
2023-01-16 13:52:39 +0100

    v3

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const yearly = document.querySelectorAll(".yearly");
 const monthly = document.querySelectorAll(".monthly");
 
 changeBtn.addEventListener("click", () => {
-    changeBtn.classList.toggle("after:left-1");
+    changeBtn.classList.toggle("after:-translate-x-7");
     yearly.forEach(x => x.classList.toggle("hidden"));
     monthly.forEach(y => y.classList.toggle("hidden"));
     
